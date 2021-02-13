094f724f9a49d0681684fe837d98181e0732efe4
Tom Eley <Tom.Eley@Gmail.com>
2021-02-13 21:33:29 +0000

    refactor

--- root/etc/cont-init.d/30-config.sh
+++ root/etc/cont-init.d/30-config.sh
@@ -5,28 +5,28 @@ CFG=/config/configFile.cfg
 
 # If environment arguments have been provided, switch the values in the sample config to these
 
-if [ -n "$PACKT_EMAIL" ]; then
+if [[ -n $PACKT_EMAIL ]]; then
 	echo 'ENV PACKT_EMAIL provided'
 	sed -i s/email=.*/email="$PACKT_EMAIL"/ $CFG
 else
 	echo 'ENV PACKTEMAIL not set'
 fi
 
-if [ -n "$PACKT_PASSWORD" ]; then
+if [[ -n $PACKT_PASSWORD ]]; then
 	echo 'ENV PACKT_PASSWORD provided'
 	sed -i s/password=.*/password="$PACKT_PASSWORD"/ $CFG
 else
 	echo 'ENV PACKTEMAIL not set'
 fi
 
-if [ -n "$PACKT_DOWNLOAD_FORMATS" ]; then
+if [[ -n $PACKT_DOWNLOAD_FORMATS ]]; then
 	echo "ENV PACKT_DOWNLOAD_FORMATS provided as '$PACKT_DOWNLOAD_FORMATS'"
 	sed -i s/download_formats:.*/download_formats:\ "$PACKT_DOWNLOAD_FORMATS"/ $CFG
 else
 	echo 'ENV PACKT_DOWNLOAD_FORMATS not set'
 fi
 
-if [ -n "$PACKT_DOWNLOAD_BOOK_TITLES" ]; then
+if [[ -n $PACKT_DOWNLOAD_BOOK_TITLES ]]; then
 	echo "ENV PACKT_DOWNLOAD_BOOK_TITLES provided as '$PACKT_DOWNLOAD_BOOK_TITLES'"
 	sed -i s/download_book_titles:.*/download_book_titles:\ "$PACKT_DOWNLOAD_BOOK_TITLES"/ $CFG
 else
@@ -34,7 +34,7 @@ else
 	sed -i s/download_book_titles:.*/download_book_titles:/ $CFG
 fi
 
-if [ -n "$PACKT_ANTICAPTCHA_KEY" ]; then
+if [[ -n $PACKT_ANTICAPTCHA_KEY ]]; then
 	echo 'ENV PACKT_ANTICAPTCHA_Key provided'
 	sed -i s/key:.*/key:\ "$PACKT_ANTICAPTCHA_KEY"/ $CFG
 else
@@ -51,7 +51,7 @@ sed -i s@ebook_extra_info_log_file_path:.*@ebook_extra_info_log_file_path:\ \\/d
 mkdir -p /config/crontabs
 
 # Copy crontab default if needed
-if [ ! -f /config/crontabs/root ]; then
+if [[ ! -f /config/crontabs/root ]]; then
 	echo 'Crontabs root file not found so creating the default'
 	cp /etc/crontabs/root /config/crontabs/
 else
